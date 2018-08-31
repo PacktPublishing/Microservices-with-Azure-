5af75406538d6c755ef0e84263075097b8602f23
Viranchis <viranchis@packtpub.com>
2018-08-31 15:19:58 +0530

    Add files via upload

---
+++ Chapter2-6/Application7/Scripts/TestChaos.ps1
@@ -0,0 +1,27 @@
+﻿$ClusterName= "stafftracker.westeurope.cloudapp.azure.com:19000"
+
+$CertThumbprint= "9CF34BB93B4EC357CECBC1E5B602F0CD36FC8C3D"
+
+Connect-ServiceFabricCluster -ConnectionEndpoint $ClusterName -KeepAliveIntervalInSec 10   -X509Credential -ServerCertThumbprint $CertThumbprint  -FindType FindByThumbprint    -FindValue $CertThumbprint -StoreLocation CurrentUser   -StoreName My
+
+$clusterHealthPolicy = New-Object -TypeName System.Fabric.Health.ClusterHealthPolicy
+$clusterHealthPolicy.MaxPercentUnhealthyNodes = 10
+$clusterHealthPolicy.MaxPercentUnhealthyApplications = 20
+$clusterHealthPolicy.ConsiderWarningAsError = $False
+$clusterHealthPolicy.ApplicationTypeHealthPolicyMap.Add("CriticalAppType", 33)
+$context = @{"k1" = "v1";"k2" = "v2"}
+Start-ServiceFabricChaos -TimeToRunMinute 6 -MaxConcurrentFaults 3 -MaxClusterStabilizationTimeoutSec 60 -WaitTimeBetweenIterationsSec 30 -WaitTimeBetweenFaultsSec 5 -EnableMoveReplicaFaults -Context $context -ClusterHealthPolicy $clusterHealthPolicy 
+
+$clusterHealthPolicy = New-Object -TypeName System.Fabric.Health.ClusterHealthPolicy
+$clusterHealthPolicy.MaxPercentUnhealthyNodes = 10
+$clusterHealthPolicy.MaxPercentUnhealthyApplications = 20
+$clusterHealthPolicy.ConsiderWarningAsError = $False
+$clusterHealthPolicy.ApplicationTypeHealthPolicyMap.Add("CriticalAppType", 33)
+$context = @{"k1" = "v1";"k2" = "v2"}
+Start-ServiceFabricChaos -TimeToRunMinute 6 -MaxConcurrentFaults 3 -MaxClusterStabilizationTimeoutSec 60 -WaitTimeBetweenIterationsSec 30 -WaitTimeBetweenFaultsSec 5 -EnableMoveReplicaFaults -Context $context -ClusterHealthPolicy $clusterHealthPolicy 
+
+
+$Now = Get-Date
+$EndTime = $Now.ToUniversalTime()
+$StartTime = $EndTime.AddMinutes(-6)
+Get-ServiceFabricChaosReport -StartTimeUtc $StartTime -EndTimeUtc $EndTime -Verbose
---
+++ Chapter2-6/backend/Controllers/StaffController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Collections;
+using Microsoft.ApplicationInsights;
+
+
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    public class StaffController : Controller
+    {
+
+        private readonly IReliableStateManager stateManager ;
+
+        private TelemetryClient telemetry = new TelemetryClient();
+
+        private object StaffDictionary;
+
+        public StaffController(IReliableStateManager stateManager)
+        {
+            this.stateManager = stateManager  ;
+        }
+
+
+        // GET api/staff
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            CancellationToken ct = new CancellationToken();
+
+            IReliableDictionary<string, int> StaffDictionary = await this.stateManager.GetOrAddAsync<IReliableDictionary<string, int>>("counts");
+
+            using (ITransaction tx = this.stateManager.CreateTransaction())
+            {
+                Microsoft.ServiceFabric.Data.IAsyncEnumerable<KeyValuePair<string, int>> list = await StaffDictionary.CreateEnumerableAsync(tx);
+
+                Microsoft.ServiceFabric.Data.IAsyncEnumerator<KeyValuePair<string, int>> enumerator = list.GetAsyncEnumerator();
+
+                List<KeyValuePair<string, int>> result = new List<KeyValuePair<string, int>>();
+
+                while (await enumerator.MoveNextAsync(ct))
+                {
+                    result.Add(enumerator.Current);
+                }
+
+                return this.Json(result);
+            }
+        }
+
+
+        // PUT api/values/staff
+        [HttpPut("{staff}")]
+        public async Task<IActionResult> Put(string staff)
+        {
+            IReliableDictionary<string, int> StaffDictionary = await this.stateManager.GetOrAddAsync<IReliableDictionary<string, int>>("counts");
+
+            using (ITransaction tx = this.stateManager.CreateTransaction())
+            {
+                await StaffDictionary.SetAsync(tx, staff, 1);
+                await tx.CommitAsync();
+            }
+            telemetry.TrackEvent($"Added a Staff for {staff}");
+            return new OkResult();
+        }
+
+        //DELETE api/values/5
+        [HttpDelete("{staff}")]
+        public async Task<IActionResult> Delete(string staff)
+        {
+
+            IReliableDictionary<string, int> StaffDictionary = await this.stateManager.GetOrAddAsync<IReliableDictionary<string, int>>("counts");
+
+            using (ITransaction tx = this.stateManager.CreateTransaction())
+            {
+                await StaffDictionary.TryRemoveAsync(tx, staff);
+                await tx.CommitAsync();
+
+                telemetry.TrackEvent($"Removed a Staff for {staff}");
+            }
+            return new OkResult() ;
+        }
+    }
+}
